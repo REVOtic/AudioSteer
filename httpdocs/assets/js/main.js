94b6629766637a6d500aa8af0b008d380bb0b910
Gaurav Kumar <developergkindia@gmail.com>
2019-05-16 00:07:46 +0530

    Upload File Added, Radio Switch Added

--- httpdocs/assets/js/main.js
+++ httpdocs/assets/js/main.js
@@ -175,28 +175,97 @@
 		},
 		data: data
 	};
-	
-	try{
+
+	try {
 		$("#chartContainer").CanvasJSChart(options);
 	} catch (err) {}
+
+	$('.btn-toggle').click(function () {
+		$(this).find('.btn').toggleClass('active');
+		$(this).find('.btn').toggleClass('btn-default');
+	});
 
 })(jQuery);
 
 
 function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
 }
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 
+
+class Switch {
+	constructor(
+		selector, {
+			highlightClass = "c-switch__highlight",
+			activeClass = "is-active"
+		} = {}) {
+		this.activeClass = activeClass;
+		this.element = document.querySelector(selector);
+		this.buttons = this.element.querySelectorAll("button");
+		this.highlight = this.element.querySelector(`.${highlightClass}`);
+		this.activeBtn = this.element.querySelector(`button.${this.activeClass}`);
+
+		if (!this.activeBtn) {
+			this.activeBtn = this.buttons[0];
+			this.activeBtn.classList.add(this.activeClass);
+		}
+
+		this._highlight();
+		this._addEvents();
+	}
+
+	_highlight() {
+		const w = this.activeBtn.offsetWidth;
+		const x = this.activeBtn.offsetLeft;
+
+		this.highlight.style.width = `${w}px`;
+		this.highlight.style.transform = `translateX(${x}px)`;
+	}
+
+	_dispatchEvent() {
+		this.element.dispatchEvent(
+			new CustomEvent("change", {
+				detail: this.activeBtn.dataset.value
+			}));
+
+	}
+
+	_addEvents() {
+		[].forEach.call(this.buttons, (btn) =>
+			btn.addEventListener("click", e => {
+				if (this.activeBtn === e.target) return;
+
+				this.activeBtn.classList.remove(this.activeClass);
+				this.activeBtn = e.target;
+				this.activeBtn.classList.add(this.activeClass);
+
+				this._highlight();
+				this._dispatchEvent();
+			}));
+
+	}
+}
+const mySwitch = new Switch(".c-switch");
+mySwitch.element.addEventListener("change", e => console.log(e.detail));
+
+
+$(function() {
+  $('input[type=file]').change(function(){
+    var t = $(this).val();
+    var labelText = 'File : ' + t.substr(12, t.length);
+    $(this).prev('label').text(labelText);
+  })
+});
