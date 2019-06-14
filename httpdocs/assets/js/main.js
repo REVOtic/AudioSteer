408fda16e79b035c61d40737f02f73479b763e1d
shachindra <shachindra.spidey@gmail.com>
2019-06-14 10:30:21 +0800

    Update

--- httpdocs/assets/js/main.js
+++ httpdocs/assets/js/main.js
@@ -145,11 +145,35 @@
 
 	var options = {
 		animationEnabled: true,
-		zoomEnabled: true,
-		zoomType: "xy",
+		//zoomEnabled: true,
+		//zoomType: "xy",
 		exportEnabled: true,
 		title: {
 			text: "Equalizer Setup"
+		},
+		draggable: true,
+		onDragStart: function(event) {
+
+		},
+		onDrag: function(event) {
+	
+		},
+		onDragEnd: function(event) {
+	
+		},
+		annotation: {
+			annotations: [
+				{
+					type: 'line',
+					mode: 'horizontal',
+					scaleID: 'y-axis-0',
+					value: 25,
+					draggable: true,
+					onDrag: function(event) {
+						console.log(event.subject.config.value);
+					}
+				}
+			]
 		},
 		axisX: {
 			logarithmic: true,
@@ -188,22 +212,22 @@
 })(jQuery);
 
 
-function openCity(evt, cityName) {
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
-	document.getElementById(cityName).style.display = "block";
-	evt.currentTarget.className += " active";
-}
+// function openCity(evt, cityName) {
+// 	var i, tabcontent, tablinks;
+// 	tabcontent = document.getElementsByClassName("tabcontent");
+// 	for (i = 0; i < tabcontent.length; i++) {
+// 		tabcontent[i].style.display = "none";
+// 	}
+// 	tablinks = document.getElementsByClassName("tablinks");
+// 	for (i = 0; i < tablinks.length; i++) {
+// 		tablinks[i].className = tablinks[i].className.replace(" active", "");
+// 	}
+// 	document.getElementById(cityName).style.display = "block";
+// 	evt.currentTarget.className += " active";
+// }
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+//document.getElementById("defaultOpen").click();
 
 
 class Switch {
